fc724498633d2ff95059c7160c7046547b4fff99
skskdmsdl <66931820+skskdmsdl@users.noreply.github.com>
2021-03-23 23:12:32 +0900

    Update ToDoItemList.js

--- src/components/ToDoItemList.js
+++ src/components/ToDoItemList.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import ToDoItem from './ToDoItem';
 
 class ToDoItemList extends Component {
-  render() {
-    const { todos, onToggle, onRemove } = this.props;
 
-    return (
-      <div>
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.todos !== nextProps.todos;
+      }  
+    render() {
+        const { todos, onToggle, onRemove } = this.props;
 
-      </div>
-    );
-  }
+        const toDoList = todos.map(
+            ({id, text, checked}) => (
+            <ToDoItem
+                id={id}
+                text={text}
+                checked={checked}
+                onToggle={onToggle}
+                onRemove={onRemove}
+                key={id}
+            />
+            )
+        );
+  
+        return (
+        <div>
+            {toDoList}    
+        </div>
+        );
+    }
 }
 
 export default ToDoItemList;
